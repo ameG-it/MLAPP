ddf76aea5ff8fc236f39e6cf780740700207a791
ameG-it <ryota_aoki@outlook.jp>
2022-07-12 23:49:24 +0900

    coreate form

---
+++ mlapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import InputForm
+
+def index(request):
+  return render(request,'mlapp/index.html')
+
+def input_form(request):
+  form = InputForm()
+  return render(request,'mlapp/input_form.html',{'form':form})
+# Create your views here.
